7eb54d5ece22de0d53ff86a834535569bf813025
Tareq Newaz Shahriar <tareq.newaz.shahriar@gmail.com>
2023-03-04 14:53:49 +0600

    build

---
+++ dist/assets/AboutView.a0e18780.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as t,a as s}from"./index.4e9ffaac.js";const c={},a=s("h1",null,"This is an about page",-1),n=[a];function _(r,i){return o(),t("div",null,n)}const d=e(c,[["render",_]]);export{d as default};
